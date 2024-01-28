d7a60fc75a4a86af6a6d352117a1b9341c1242b4
Jeremy Pittard <jeremy@jpittard.net>
2024-01-28 12:56:36 +0800

    attempt 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), markdoc(), keystatic()],
+  output: "hybrid",
+  adapter: vercel()
+});
